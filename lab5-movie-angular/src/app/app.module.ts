accb60b0cb4d618b6c60264e980dc69a60bff53a
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-21 21:56:41 +0530

    Added required modules

--- lab5-movie-angular/src/app/app.module.ts
+++ lab5-movie-angular/src/app/app.module.ts
@@ -1,3 +1,10 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-04-21 15:50:18
+ * @modify date 2020-04-21 15:50:18
+ * @desc Root Module
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,16 +12,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { HomeComponent } from './home/home.component';
+import { BarRatingModule } from "ngx-bar-rating";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
     HomeComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BarRatingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
