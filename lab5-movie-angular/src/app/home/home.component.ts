f457d33f79947d7829ce3da658e360cfd635a9ba
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-21 23:06:35 +0530

    Implemented hoem compoennet logic

--- lab5-movie-angular/src/app/home/home.component.ts
+++ lab5-movie-angular/src/app/home/home.component.ts
@@ -1,15 +1,41 @@
+import { Genre } from './../model/genre.model';
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-04-21 17:28:16
+ * @modify date 2020-04-21 17:28:16
+ * @desc Home Component
+ */
+import { Movie } from './../model/movie.model';
+import { MovieService } from './../service/movie.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  movies: Movie[] = [];
+  genres = Genre;
 
-  constructor() { }
+  constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
+    this.fetchAllMovies();
+    console.log(this.genres);
+
   }
 
+  public fetchAllMovies() {
+    this.movieService.fetchAllMovies().subscribe((response) => {
+      this.movies = response;
+    });
+  }
+
+  public fetchByGenre(genre: string){
+    this.movieService.fetchByGenre(genre).subscribe(response=>{
+      this.movies = response;
+    });
+  }
 }
