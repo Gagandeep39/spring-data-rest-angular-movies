266d9973a056f64676f80816464e4dd71821ffe8
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-21 23:46:59 +0530

    Added a method to add a movie

--- lab5-movie-angular/src/app/service/movie.service.ts
+++ lab5-movie-angular/src/app/service/movie.service.ts
@@ -1,3 +1,11 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-04-21 23:40:45
+ * @modify date 2020-04-21 23:40:45
+ * @desc [description]
+ */
+import { Movie } from './../model/movie.model';
 import { map } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment.prod';
@@ -26,6 +34,9 @@ export class MovieService {
     return this.http
       .get(environment.url + environment.fetchByGenre + genre)
       .pipe(map((response) => response['_embedded']['movies']));
+  }
 
+  public addMovie(movie: Movie){
+    return this.http.post(environment.url, movie);
   }
 }
