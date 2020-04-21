5eaba948e76bb659a1573d1afe5cece5db675a7b
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-04-21 21:55:25 +0530

    Added a method to fetch by Genre

--- lab5-movie-spring-boot/src/main/java/com/gagan/lab5moviespringboot/repository/MovieRepository.java
+++ lab5-movie-spring-boot/src/main/java/com/gagan/lab5moviespringboot/repository/MovieRepository.java
@@ -12,13 +12,27 @@ package com.gagan.lab5moviespringboot.repository;
 import com.gagan.lab5moviespringboot.entity.Movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
 /**
  * To allow communication with lcoalhost4200
+ * @CrossOrigin("http://localhost:4200")
  */
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    /**
+     * Custom REST impl
+     * @param genre Searches a movie by Genre
+     * @return  List of all movies mathicng this genre
+     * @apiNote returnType findBy{PropertyNameOfEntity}(@Param("param1")String param1, @Param("paramX")String paramX)
+     */
+    public List<Movie> findByMoviesGenre(@Param("genre") String genre);
 
 }
